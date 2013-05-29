06da1a1bffa73b03dc446e938c4464c6ed7791ff
i09158knct <i09158knct@gmail.com>
2013-05-29 23:18:41 +0900

    implement animal.scm

---
+++ rep2/animal.scm
@@ -0,0 +1,41 @@
+(define *init-base*
+  '("背骨があるか"
+    ("温血動物か"
+     ("こどもを母乳で育てるか"
+      ("水中に住んでいるか"
+       "いるか"
+       ("一般に飼い慣らされているか"
+        "いぬ"
+        "とら"))
+      ("飛べるか"
+       "こまどり"
+       "にわとり"))
+     ("えらを持っていて、一生水中で生活するか"
+      "まぐろ"
+      ("最初はえらで、そのうち肺呼吸をするようになるか"
+       "かえる"
+       ("足があるか"
+        "わに"
+        "へび"))))
+    ("飛べるか"
+     "か"
+     "みみず")))
+
+(define (yes-or-no? prompt)
+  (display prompt) (display " (y/n)? ")
+  (if (eq? (read) 'y) #t #f))
+
+(define (animal tree)
+  (if (list? tree)
+      (if (yes-or-no? (car tree))
+          (animal (cadr tree))
+          (animal (caddr tree)))
+      (begin (print tree) tree)))
+
+
+(define (start)
+  (newline) (display "### アニマル プログラム ###")
+  (newline) (newline)
+  (animal *init-base*))
+
+(start)
